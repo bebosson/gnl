32ccb677b53d63ad0312a446c9144376424329c8
Benjamin BOSSON <bebosson@e1r8p2.42.fr>
2018-12-09 19:26:01 +0100

    -FINAL

--- get_next_line.c
+++ get_next_line.c
@@ -6,62 +6,57 @@
 /*   By: bebosson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/08 16:21:08 by bebosson          #+#    #+#             */
-/*   Updated: 2018/12/08 20:35:53 by bebosson         ###   ########.fr       */
+/*   Updated: 2018/12/09 18:46:00 by bebosson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-#include "libft/incs/libft.h"
+#include "libft/libft.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <limits.h>
 
-char		*ft_strsub2(const char *s,unsigned int start, size_t len)
+int		get_next_line1(const int fd, char **line, char *tmp[OPEN_MAX])
 {
-	char	*new;
-	if (!(new = ft_strsub(s,start,len)))
-		return (NULL);
-	free((char *)s);
-	return (new);
-}
+	int		i;
+	char	*tmp2;
 
-char*ft_strjoin2(const char *s1, const char *s2)
-{
-	char	*new;
-	if (!(new = ft_strjoin(s1,s2)))
-		return (NULL);
-	free((char *)s1);
-	return (new);
-}
-
-int		get_next_line(const int fd, char **line)
-{
-	int			ret;
-	char		buff[BUFF_SIZE + 1];
-	static char	*tmp[4096];
-	int			i;
-
-	if (fd < 0 || (read(fd, buff, 0) < 0) || BUFF_SIZE < 1)
-		return (-1);
-	if (!tmp[fd])
-		tmp[fd] = malloc(sizeof(*tmp) * 2);
-	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
-	{
-		buff[ret] = '\0';
-		tmp[fd] = ft_strjoin2(tmp[fd], buff);
-		if (ft_strchr(buff, '\n'))
-			break ;
-	}
-	if (ret < 0)
-		return (-1);
 	i = 0;
 	while (tmp[fd][i] && tmp[fd][i] != '\n')
 		i++;
 	*line = ft_strsub(tmp[fd], 0, i);
 	if (ft_strlen(tmp[fd]))
 	{
-		tmp[fd] = ft_strsub2(tmp[fd], i + 1, ft_strlen(tmp[fd]) - ft_strlen(*line));
+		tmp2 = tmp[fd];
+		tmp[fd] = ft_strsub(tmp[fd], i + 1, ft_strlen(tmp[fd]) - i);
+		free(tmp2);
 		return (1);
 	}
 	return (0);
+}
+
+int		get_next_line(const int fd, char **line)
+{
+	int			ret;
+	char		buff[BUFF_SIZE + 1];
+	static char	*tmp[OPEN_MAX];
+	char		*tmp2;
+
+	if (fd < 0 || (read(fd, buff, 0) < 0) || BUFF_SIZE < 1)
+		return (-1);
+	if (!tmp[fd])
+		tmp[fd] = ft_memalloc(BUFF_SIZE + 1);
+	while ((ret = read(fd, buff, BUFF_SIZE)) > 0)
+	{
+		buff[ret] = '\0';
+		tmp2 = tmp[fd];
+		tmp[fd] = ft_strjoin(tmp[fd], buff);
+		free(tmp2);
+		if (ft_strchr(buff, '\n'))
+			break ;
+	}
+	if (ret < 0)
+		return (-1);
+	return (get_next_line1(fd, &(*line), tmp));
 }
--- get_next_line.h
+++ get_next_line.h
@@ -6,13 +6,13 @@
 /*   By: bebosson <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/12/04 20:17:19 by bebosson          #+#    #+#             */
-/*   Updated: 2018/12/08 20:18:24 by bebosson         ###   ########.fr       */
+/*   Updated: 2018/12/09 16:53:19 by bebosson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
-# define BUFF_SIZE 1
+# define BUFF_SIZE 20
 # include <string.h>
 # include <unistd.h>
 # include <stdio.h>
